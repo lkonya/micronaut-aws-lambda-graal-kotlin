b062f0b87a18c8a1c1640b0dc4e872d119b4129c
Laszlo Konya <laszlo.konya@emarsys.com>
2019-03-29 00:04:51 +0100

    refactor

--- src/main/kotlin/aws/lambda/graal/kotlin/controller/RssController.kt
+++ src/main/kotlin/aws/lambda/graal/kotlin/controller/RssController.kt
@@ -1,48 +1,24 @@
 package aws.lambda.graal.kotlin.controller
 
 import aws.lambda.graal.kotlin.client.RssFeedClient
-import aws.lambda.graal.kotlin.model.RssFeed
-import com.sun.syndication.feed.synd.SyndCategoryImpl
-import com.sun.syndication.feed.synd.SyndEntryImpl
-import com.sun.syndication.feed.synd.SyndFeed
+import aws.lambda.graal.kotlin.model.RssFeeds
+import aws.lambda.graal.kotlin.model.toRssFeeds
+import aws.lambda.graal.kotlin.model.typedEntries
 import io.micronaut.http.MediaType.TEXT_XML
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.reactivex.Flowable
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.util.*
 
 @Controller("/rss")
 class RssController(private val feedClient: RssFeedClient) {
 
+
     @Get("/feed")
-    fun feed(): Flowable<List<RssFeed>> = feedClient.fetchRssFromRaw().map { toRssFeed(it) }
+    fun feed(): Flowable<RssFeeds> =
+            feedClient.fetchRssFromRaw()
+                    .map { it.typedEntries().toRssFeeds() }
 
     @Get(value = "/raw", produces = [TEXT_XML])
     fun raw(): Flowable<String> = feedClient.fetchRawRss()
 
-    private fun toRssFeed(feed: SyndFeed): List<RssFeed> {
-        return feed.typedEntries().mapNotNull { it }
-                .map {
-                    RssFeed(
-                            it.description?.value ?: "",
-                            it.typedCategories().mapNotNull { category -> category?.name },
-                            it.link,
-                            it.publishedDate.toLocalDateTime()
-                    )
-                }
-    }
-
-    private fun SyndFeed.typedEntries(): List<SyndEntryImpl?> =
-            this.entries as List<SyndEntryImpl?>
-
-    private fun SyndEntryImpl.typedCategories(): List<SyndCategoryImpl?> =
-            this.categories as List<SyndCategoryImpl?>
-
-    private fun Date?.toLocalDateTime(): LocalDateTime =
-            this?.run {
-                Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalDateTime()
-            } ?: LocalDateTime.now()
 }
--- src/main/kotlin/aws/lambda/graal/kotlin/model/RssFeed.kt
+++ src/main/kotlin/aws/lambda/graal/kotlin/model/RssFeed.kt
@@ -1,5 +1,36 @@
 package aws.lambda.graal.kotlin.model
 
+import com.sun.syndication.feed.synd.SyndCategoryImpl
+import com.sun.syndication.feed.synd.SyndEntry
+import com.sun.syndication.feed.synd.SyndFeed
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 
+data class RssFeeds(val feeds: List<RssFeed>)
 data class RssFeed(val description: String, val categories: List<String>, val link: String, val publishedDate: LocalDateTime)
+
+fun List<SyndEntry>.toRssFeeds(): RssFeeds {
+    val feeds = this.map {
+        RssFeed(
+                it.description?.value ?: "",
+                it.typedCategories().mapNotNull { category -> category.name },
+                it.link,
+                it.publishedDate.toLocalDateTime()
+        )
+    }
+    return RssFeeds(feeds)
+}
+
+
+internal fun SyndFeed.typedEntries(): List<SyndEntry> =
+        (this.entries as List<SyndEntry?>).mapNotNull { it }
+
+private fun SyndEntry.typedCategories(): List<SyndCategoryImpl> =
+        (this.categories as List<SyndCategoryImpl?>).mapNotNull { it }
+
+private fun Date?.toLocalDateTime(): LocalDateTime =
+        this?.run {
+            Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalDateTime()
+        } ?: LocalDateTime.now()
